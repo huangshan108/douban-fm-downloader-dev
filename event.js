11843cab020fa4c5dddfd9721c7988dea68229ef
ShanHuang <shanhuang108@gmail.com>
2014-08-12 00:15:05 -0700

    button added

--- event.js
+++ event.js
@@ -1,5 +1,27 @@
+// chrome.browserAction.onClicked.addListener(function(tab) {
+// 	var id;
+// 	chrome.tabs.getSelected(null, function (tab) {  
+// 		id=tab.id;
+// 	}); 
+// 	chrome.tabs.executeScript(id, {file: "jquery-1.8.3.min.js"}, function(){
+// 	    chrome.tabs.executeScript(id, {file: "hack.js"}, function(){
+
+// 	    });
+// 	});
+// });
+
+// $("#simulate-sec").prepend('<button id="download" style="margin:0; width:100px; height:26px;background:none;border:1px solid #9DD6C5;outline:none; color:#65C39E; position:relative; right: -90;left: 240px;" id="download">下载这首歌</button>');
+// $("#download").mouseover(function() {
+// 	$("#download").css({"background-color":"#9DD6C5", "color":"white", "-o-transition":".2s", "-ms-transition":".2s","-moz-transition":".2s","-webkit-transition":".2s","transition":".2s"});
+// });
+// $("#download").mouseout(function() {
+// 	$('#download').css({"background-color":"transparent", "color":"#65C39E","-o-transition":".2s", "-ms-transition":".2s","-moz-transition":".2s","-webkit-transition":".2s","transition":".2s"});
+// });
+
+// var script = document.createElement("script");
+// script.appendChild(document.createTextNode("window.extStatusHandler = function(a) {var o = jQuery.parseJSON(a);console.log(o.song.url);};"));
+// (document.body || document.head || document.documentElement).appendChild(script);alert("Script Enjected Successfully!");
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    code: 'var script = document.createElement("script");script.appendChild(document.createTextNode("window.extStatusHandler = function(a) {var o = jQuery.parseJSON(a);console.log(o.song.url);};"));(document.body || document.head || document.documentElement).appendChild(script);alert("Script Enjected Successfully!");'
-  });
+  chrome.tabs.executeScript(null, {file: "content_script.js"});
 });
---
+++ hack.js
@@ -0,0 +1,15 @@
+jQuery("#simulate-sec").prepend('<a href="#" target="_blank" style="position: relative; left: 240px; background:none"><button id="download" style="margin:0; width:100px; height:26px;background:none;border:1.5px solid #9DD6C5;outline:none; color:#65C39E;" id="download">下载这首歌</button></a>');
+jQuery("#download").mouseover(function() {
+	jQuery("#download").css({"background-color":"#9DD6C5", "color":"white", "-o-transition":".2s", "-ms-transition":".2s","-moz-transition":".2s","-webkit-transition":".2s","transition":".2s"});
+});
+jQuery("#download").mouseout(function() {
+	jQuery('#download').css({"background-color":"transparent", "color":"#65C39E","-o-transition":".2s", "-ms-transition":".2s","-moz-transition":".2s","-webkit-transition":".2s","transition":".2s"});
+});
+jQuery(document).ready(function() {
+    var script = document.createElement("script");
+script.appendChild(document.createTextNode("window.extStatusHandler = function(a) {var o = jQuery.parseJSON(a);console.log(o.song.url);};"));
+(document.body || document.head || document.documentElement).appendChild(script);
+// alert("Script Enjected Successfully!");
+});
+
+// jQuery('#download a').attr('href', o.song.url);
